5ae5158a173b0385bdf2485deba9720c7552e771
Danny <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-04-21 22:33:50 +0100

    feat: created simple API endpoints

--- src/v1/games/games.controller.ts
+++ src/v1/games/games.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Req } from '@nestjs/common';
 
 @Controller('games')
-export class GamesController {}
+export class GamesController {
+  @Get()
+  findAll(@Req() request: Request): string {
+    return 'This action returns all games';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} game`;
+  }
+
+  @Post()
+  create(): string {
+    return 'This action adds a new game';
+  }
+}
