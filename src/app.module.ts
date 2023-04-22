a217ddd012fb72a0016ae27e4f513e3d7ec0c222
Danny <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-04-22 19:02:36 +0100

    feat: created sets mo, s, co

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { GamesModule } from './v1/games/games.module';
 import { PublicModule } from './v1/public/public.module';
 import { SeriesEntity } from './v1/series/entities/series.entity';
 import { SeriesModule } from './v1/series/series.module';
+import { SetsModule } from './v1/sets/sets.module';
 
 @Module({
   imports: [
@@ -25,6 +26,7 @@ import { SeriesModule } from './v1/series/series.module';
     PublicModule,
     GamesModule,
     SeriesModule,
+    SetsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
