e717352ca60b28c2b9ca7bdc9468a2869ca9ae13
Danny O'Neill-James <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-04-22 11:52:34 +0100

    feat: Set up database (#2)
    * chore: installed typeORM and Mysql2 created .env
    
    * feat: connects to database
    
    * feat: set up game entity for use.

--- src/v1/games/games.service.ts
+++ src/v1/games/games.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateGameDto } from './dto/create-game.dto';
+import { GameEntity } from './entities/game.entity';
 import { IGame } from './interface/games.interface';
 
 @Injectable()
 export class GamesService {
+  constructor(
+    @InjectRepository(GameEntity)
+    private usersRepository: Repository<GameEntity>,
+  ) {}
+
   async findOne(id: string): Promise<IGame> {
     const game: IGame = {
       name: 'Returned Single Game',
