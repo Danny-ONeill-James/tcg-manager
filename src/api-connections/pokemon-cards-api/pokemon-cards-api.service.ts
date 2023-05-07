341572007b4d1aa67485a1bdee55687dcb566b58
Danny <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-05-07 21:16:52 +0100

    feat: added axios command

--- src/api-connections/pokemon-cards-api/pokemon-cards-api.service.ts
+++ src/api-connections/pokemon-cards-api/pokemon-cards-api.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import axios from 'axios';
+import { CardEntity } from 'src/v1/cards/entities/card.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PokemonCardsApiService {}
+export class PokemonCardsApiService {
+  constructor(
+    @InjectRepository(CardEntity)
+    private cardsRepository: Repository<CardEntity>,
+  ) {}
+
+  async getSetsList() {
+    const config = {
+      headers: {
+        'X-Api-Key': process.env.POKEMON_TCG_IO_KEY,
+      },
+    };
+    const url = `https://api.pokemontcg.io/v2/sets`;
+
+    return await axios
+      .get(url, config)
+      .then((res) => {
+        console.log(res);
+        return res;
+      })
+      .catch((err) => console.log(err));
+  }
+}
