c6fc976cf5a14e096a3c101b4fc7aaad8b6f068f
Danny <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-05-08 13:39:58 +0100

    feat: create users. Will make more secure later

--- src/system/users/users.service.ts
+++ src/system/users/users.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { UserEntity } from './entities/user.entity';
+import { IUser } from './interfaces/user.interface';
+import { async } from 'rxjs';
 
 export type User = any;
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {}
   private readonly users = [
     {
       userId: 1,
@@ -19,5 +29,13 @@ export class UsersService {
 
   async findOne(username: string): Promise<User | undefined> {
     return this.users.find((user) => user.username === username);
+  }
+
+  async create(newUserDto: CreateUserDto): Promise<IUser> {
+    const newUser = this.userRepository.create({
+      ...newUserDto,
+    });
+
+    return this.userRepository.save(newUser);
   }
 }
