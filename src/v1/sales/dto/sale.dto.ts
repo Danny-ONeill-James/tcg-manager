7b42ee80551a8ac62d63bbb7e8f83572fdd0f77f
Danny <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-05-17 15:30:30 +0100

    feat: added fields in input

--- src/v1/sales/dto/sale.dto.ts
+++ src/v1/sales/dto/sale.dto.ts
@@ -1,9 +1,14 @@
 import { UserEntity } from 'src/system/users/entities/user.entity';
 import { SaleItemEntity } from '../entities/saleItem.entity';
 import { TransactionEntity } from '../entities/transaction.entity';
+import { ECompany } from '../enums/company.enum';
+import { EStatus } from '../enums/status.enum';
 
 export class CreateSaleDto {
   id: string;
+  company: ECompany;
+  status: EStatus;
+  saleDate: Date;
   saleItem?: SaleItemEntity[];
   transaction: TransactionEntity[];
   user?: UserEntity;
