ecd751dc5543f18b8d0761bfce0e63d8fc49e9eb
danny O'Neill-James <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-06-06 11:23:19 +0100

    wip setting up binder controller

--- src/v1/binder/binder.controller.ts
+++ src/v1/binder/binder.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { BinderService } from './binder.service';
+import { InputBinderDto } from './dto/binder.dto';
+import { IBinder } from './interface/binder.interface';
 
 @Controller('binder')
-export class BinderController {}
+export class BinderController {
+  constructor(private binderService: BinderService) {}
+
+  @UseGuards(AuthGuard)
+  @Post(':userId')
+  create(
+    @Param('userId') userId: string,
+    @Body() createBinderDto: InputBinderDto[],
+  ): Promise<IBinder> {
+    return this.binderService.createBinder(userId, createBinderDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get(':userId/:binderSlug')
+  getAllBindersForUser(
+    @Param('userId') userId: string,
+    @Param('binderSlug') binderSlug: string,
+  ): Promise<IBinder[]> {
+    return this.binderService.getAllBindersForUser(userId, binderSlug);
+    return null;
+  }
+}
--- src/v1/binder/binder.service.ts
+++ src/v1/binder/binder.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { IBinder } from './interface/binder.interface';
+import { InputBinderDto } from './dto/binder.dto';
 
 @Injectable()
-export class BinderService {}
+export class BinderService {
+  getAllBindersForUser(userId: string, binderSlug: string): Promise<IBinder[]> {
+    throw new Error('Method not implemented.');
+  }
+  createBinder(
+    userId: string,
+    createBinderDto: InputBinderDto[],
+  ): Promise<IBinder> {
+    throw new Error('Method not implemented.');
+  }
+}
