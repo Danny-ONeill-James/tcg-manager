3412aeba09c5d76d7b2dd1a51013808101721e1b
Danny O'Neill-James <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-08-08 14:06:01 +0100

    feat: changed to postgress from Mysql (#34)

--- src/app.module.ts
+++ src/app.module.ts
@@ -35,9 +35,9 @@ import { VendorsModule } from './vendors/vendors.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: 'postgres',
       host: process.env.MYSQL_HOST,
-      port: 3306,
+      port: 5432,
       username: process.env.MYSQL_USERNAME,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE,
